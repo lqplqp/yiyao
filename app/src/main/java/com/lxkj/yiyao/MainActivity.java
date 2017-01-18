532c709e8c26cce1eeeecc979d5b1c4ddb11d2aa
sun <sunzhan2016@163.com>
2017-01-18 23:34:31 +0800

    sun 2017年1月18日23:34:02  侧滑半成品

--- app/src/main/java/com/lxkj/yiyao/MainActivity.java
+++ app/src/main/java/com/lxkj/yiyao/MainActivity.java
@@ -1,10 +1,14 @@
 package com.lxkj.yiyao;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.lxkj.yiyao.adapter.VPFAdapter;
 
@@ -19,8 +23,13 @@ public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.navi_view)
+    NavigationView naviView;
+    @BindView(R.id.drawer)
+    DrawerLayout drawer;
 
     private String[] shengjiTabTitles = {"首页", "审核"};
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +37,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerToggle = new ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close);
+        drawerToggle.syncState();
+        drawer.setDrawerListener(drawerToggle);
         viewPager.setAdapter(new VPFAdapter(getSupportFragmentManager(), 0, shengjiTabTitles));
         tab.setupWithViewPager(viewPager);
         tab.setTabMode(TabLayout.MODE_FIXED);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
