a1de4733fa5647b79b667f7159a4894d49d2a9a0
sun <sunzhan2016@163.com>
2017-03-06 21:58:29 +0800

    sun 2017年3月6日21:58:11 省级做一部分

--- app/src/main/java/com/lxkj/yiyao/shengji/JianGuanTongJiFragment.java
+++ app/src/main/java/com/lxkj/yiyao/shengji/JianGuanTongJiFragment.java
@@ -1,7 +1,18 @@
 package com.lxkj.yiyao.shengji;
 
+import android.util.Log;
+
 import com.lxkj.yiyao.R;
 import com.lxkj.yiyao.base.BaseFragment;
+import com.lxkj.yiyao.global.GlobalString;
+import com.lxkj.yiyao.shengji.adapter.MessageSearchAdapter;
+import com.lxkj.yiyao.view.RefreshListView;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
+
+import butterknife.BindView;
 
 /**
  * Created by Administrator on 2017/1/19.
@@ -9,10 +20,96 @@ import com.lxkj.yiyao.base.BaseFragment;
 
 public class JianGuanTongJiFragment extends BaseFragment {
 
+    // ======================== 模板代码=============================
+
+    MessageSearchAdapter adapter;
+    @BindView(R.id.list_view)
+    RefreshListView listView;
+    private int page = 1;
+
+    private String TAG = "CompanyManageyFragment";
+
+
     @Override
     protected void initView() {
 
+
+        listView.setOnRefreshListener(new RefreshListView.OnRefreshListener() {
+            @Override
+            public void onDownPullRefresh() {
+
+
+                adapter.clear();
+                adapter.notifyDataSetChanged();
+                page = 1;
+
+
+                requestData();
+
+
+            }
+
+            @Override
+            public void onLoadingMore() {
+
+
+                requestData();
+
+
+            }
+        });
+
+
     }
+    // ======================== 模板代码=============================
+
+
+    // ======================== 模板代码=============================
+    public void requestData() {
+        RequestParams params = new RequestParams(GlobalString.BaseURL + GlobalString.fenji1_tzxx);
+        params.addBodyParameter("page", page + "");
+
+        x.http().get(params, new Callback.CacheCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Log.i(TAG, result);
+                if (adapter == null) {
+                    adapter = new MessageSearchAdapter(result);
+                    listView.setAdapter(adapter);
+                } else {
+                    adapter.addData(result);
+                    listView.deferNotifyDataSetChanged();
+                }
+                page++;
+
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                ex.printStackTrace();
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+                listView.onRefreshComplete();
+                listView.loadMoreComplete();
+            }
+
+            @Override
+            public boolean onCache(String result) {
+                return false;
+            }
+        });
+    }
+
+
+    // ======================== 模板代码=============================
+
 
     @Override
     public int getLayout() {
