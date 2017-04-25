eed007f9cef0b41cd0a9b5b1e3ba041245ff78c7
sun <sunzhan2016@163.com>
2017-04-25 12:29:40 +0800

    sun 2017年4月25日12:29:04  县级管理员选购培训项目

--- app/src/main/java/com/lxkj/yiyao/xianji/QYManagerHomeFragment.java
+++ app/src/main/java/com/lxkj/yiyao/xianji/QYManagerHomeFragment.java
@@ -1,9 +1,11 @@
 package com.lxkj.yiyao.xianji;
 
 import android.content.Intent;
+import android.view.View;
 import android.widget.TextView;
 
 import com.lxkj.yiyao.R;
+import com.lxkj.yiyao.activity.SelectTrainActivity;
 import com.lxkj.yiyao.base.BaseFragment;
 import com.lxkj.yiyao.jianguan.AddAdminActivity;
 
@@ -37,9 +39,19 @@ public class QYManagerHomeFragment extends BaseFragment {
         return R.layout.qymanager_fragment_layout_home;
     }
 
-    @OnClick(R.id.add_people)
-    public void onClick() {
-        Intent intent = new Intent(getActivity(), AddAdminActivity.class);
-        startActivity(intent);
+    @OnClick({R.id.add_people, R.id.select_project})
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.add_people:
+                intent = new Intent(getActivity(), AddAdminActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.select_project:
+                intent = new Intent(getActivity(), SelectTrainActivity.class);
+                intent.putExtra("qiye_admin", true);
+                startActivity(intent);
+                break;
+        }
     }
 }
